2499ed8f8bf8aabe0c601bd3a65dce7620955673
liliwang <monsi0510@hotmail.com>
2020-11-25 20:25:09 -0600

    Refactor roll dice exercise to bonus question

--- src/main/java/com/codeup/myblog/RollDiceController.java
+++ src/main/java/com/codeup/myblog/RollDiceController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+
 @Controller
 public class RollDiceController {
 
@@ -15,9 +17,18 @@ public class RollDiceController {
 
     @GetMapping("/roll-dice/{guess}")
     public String showGuessResult(@PathVariable int guess, Model model) {
-        int dice = (int) (Math.random() * (6 - 1 + 1) + 1);
-        model.addAttribute("dice", dice);
+        ArrayList<Integer> dices = new ArrayList<>();
+        dices.add((int) (Math.random() * (6 - 1 + 1) + 1));
+        dices.add((int) (Math.random() * (6 - 1 + 1) + 1));
+        dices.add((int) (Math.random() * (6 - 1 + 1) + 1));
+        int counter = 0;
+        for ( int dice: dices) {
+            if (guess == dice)
+                counter++;
+        }
+        model.addAttribute("dices", dices);
         model.addAttribute("guess", guess);
+        model.addAttribute("counter", counter);
         return "show-dice-result";
     }
 
