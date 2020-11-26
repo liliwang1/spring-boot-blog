43bc50d0ce613f91582121cdd4cdfe72ecb5c1f7
liliwang <monsi0510@hotmail.com>
2020-11-25 21:01:01 -0600

    Add views for posts and individual post

--- src/main/java/com/codeup/myblog/PostController.java
+++ src/main/java/com/codeup/myblog/PostController.java
@@ -1,24 +1,31 @@
 package com.codeup.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postsIndex() {
-        return "posts index page";
+    public String postsIndex(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(new Post(1, "Post 1", "This is the first post."));
+        posts.add(new Post(2, "Post 2", "This is the second post."));
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String post(@PathVariable long id) {
-        return "individual post" + id;
+    public String post(@PathVariable long id, Model model) {
+        Post post = new Post(3, "Post 3", "This is the third post.");
+        model.addAttribute("post", post);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
