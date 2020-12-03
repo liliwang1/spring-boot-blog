ae38006f8820f683fa8391574d5015dbf29799f5
liliwang <monsi0510@hotmail.com>
2020-12-03 02:00:51 -0600

    Add one-to-many relationship between User and Post

--- src/main/java/com/codeup/myblog/models/Post.java
+++ src/main/java/com/codeup/myblog/models/Post.java
@@ -15,6 +15,31 @@ public class Post {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
+   @ManyToOne
+   @JoinColumn(name = "user_id")
+   private User user;
+
+    public Post(long id, String title, String body, User user) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public Post(String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Post(long id, String title, String body) {
         this.id = id;
         this.title = title;
