c90e8b354b463676869c2f0939a3cfa90607f819
liliwang <monsi0510@hotmail.com>
2020-12-11 00:22:24 -0600

    Fix fetch url

--- src/main/resources/static/js/posts.js
+++ src/main/resources/static/js/posts.js
@@ -1,12 +1,15 @@
 const renderDiv = document.getElementById('posts');
-const url = 'localhost:8080/posts.json';
-const getPosts = () => fetch(url)
-    .then(res => res.json())
-    .then(posts => {
-        console.log(posts);
-        posts.forEach(post => renderPost(post))
-    })
-    .catch(err => console.log(err));
+const url = '/posts.json';
+
+const getPosts = () => {
+    fetch(url)
+        .then(res => res.json())
+        .then(posts => {
+            console.log(posts);
+            posts.forEach(post => renderPost(post))
+        })
+        .catch(err => console.log(err));
+}
 
 const renderPost = (post) => {
     let output = `
@@ -14,7 +17,10 @@ const renderPost = (post) => {
         <h1>${post.title}</h1>
         <p>${post.body}</p>
         <p>published by ${post.user.userName}</p>
+        <br>
     </div>
     `;
     renderDiv.innerHTML += output;
 }
+
+getPosts();
